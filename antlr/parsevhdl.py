a57d46018639a47cfaa1ee05b8bd5c4e2e9a7643
Maurice van der Pot <griffon26@kfk4ever.com>
2017-03-26 12:13:30 +0200

    Completed basic antlr parser for turning vhdl into json

--- antlr/parsevhdl.py
+++ antlr/parsevhdl.py
@@ -32,24 +32,28 @@ class myVhdlVisitor(vhdlVisitor):
         return self.visit(ctx.library_unit())
 
     def visitLibrary_unit(self, ctx:vhdlParser.Library_unitContext):
-        result = {}
         if ctx.primary_unit():
-            result.update(self.visit(ctx.primary_unit()))
-        if ctx.secondary_unit():
-            result.update(self.visit(ctx.secondary_unit()))
-        return result
+            return self.visit(ctx.primary_unit())
+        elif ctx.secondary_unit():
+            return self.visit(ctx.secondary_unit())
+        else:
+            raise RuntimeError('Oops')
 
     def visitPrimary_unit(self, ctx:vhdlParser.Primary_unitContext):
         if ctx.entity_declaration():
-            return self.visit(ctx.entity_declaration())
+            result = { 'type' : 'entity_declaration' }
+            result.update(self.visit(ctx.entity_declaration()))
+            return result
         else:
-            return {}
+            raise RuntimeError('Oops')
 
     def visitSecondary_unit(self, ctx:vhdlParser.Secondary_unitContext):
         if ctx.architecture_body():
-            return self.visit(ctx.architecture_body())
+            result = { 'type' : 'architecture_body' }
+            result.update(self.visit(ctx.architecture_body()))
+            return result
         else:
-            return {}
+            raise RuntimeError('Oops')
 
     def visitEntity_declaration(self, ctx:vhdlParser.Entity_declarationContext):
         result = { 'name' : self.visit(ctx.identifier(0)) }
@@ -79,7 +83,6 @@ class myVhdlVisitor(vhdlVisitor):
 
         for itemctx in ctx.block_declarative_item():
             item = self.visit(itemctx)
-            print(item)
             if item['type'] == 'component_declaration':
                 result['components'].append(item['value'])
             elif item['type'] == 'signal_declaration':
@@ -90,13 +93,49 @@ class myVhdlVisitor(vhdlVisitor):
         return result
 
     def visitArchitecture_statement_part(self, ctx:vhdlParser.Architecture_statement_partContext):
-        return [self.visit(c) for c in ctx.architecture_statement()]
+        result = {
+            'component_instantiations' : []
+        }
+        for itemctx in ctx.architecture_statement():
+            item = self.visit(itemctx)
+            if item['type'] == 'component_instantiation':
+                result['component_instantiations'].append(item['value'])
+            else:
+                raise RuntimeError('Oops')
+        return result
 
     def visitArchitecture_statement(self, ctx:vhdlParser.Architecture_statementContext):
         if ctx.component_instantiation_statement():
-            return self.visit(ctx.component_instantiation_statement())
+            return { 'type' : 'component_instantiation',
+                     'value' : self.visit(ctx.component_instantiation_statement()) }
         else:
             return {}
+
+    def visitComponent_instantiation_statement(self, ctx:vhdlParser.Component_instantiation_statementContext):
+        if ctx.port_map_aspect():
+            portmap = self.visit(ctx.port_map_aspect())
+        else:
+            portmap = []
+        return { 'instancename' : self.visit(ctx.label_colon().identifier()),
+                 'componentname' : self.visit(ctx.instantiated_unit().name()),
+                 'portmap' : portmap }
+
+
+    def visitPort_map_aspect(self, ctx:vhdlParser.Port_map_aspectContext):
+        return self.visit(ctx.association_list())
+
+    def visitAssociation_list(self, ctx:vhdlParser.Association_listContext):
+        values = [self.visit(c) for c in ctx.association_element()]
+        return {k: v for k,v in values}
+
+    def visitAssociation_element(self, ctx:vhdlParser.Association_elementContext):
+        return (self.visit(ctx.formal_part()), self.visit(ctx.actual_part()))
+
+    def visitFormal_part(self, ctx:vhdlParser.Formal_partContext):
+        return ctx.getText()
+
+    def visitActual_part(self, ctx:vhdlParser.Actual_partContext):
+        return ctx.getText()
 
     def visitBlock_declarative_item(self, ctx:vhdlParser.Block_declarative_itemContext):
         if ctx.component_declaration():
@@ -107,6 +146,10 @@ class myVhdlVisitor(vhdlVisitor):
                      'value' : self.visit(ctx.signal_declaration()) }
         else:
             raise RuntimeError('Oops')
+
+    def visitSignal_declaration(self, ctx:vhdlParser.Signal_declarationContext):
+        return { 'name' : self.visit(ctx.identifier_list()),
+                 'subtype_indication' : self.visit(ctx.subtype_indication()) }
 
     def visitComponent_declaration(self, ctx:vhdlParser.Component_declarationContext):
         result = { 'name' : self.visit(ctx.identifier(0))}
@@ -125,7 +168,16 @@ class myVhdlVisitor(vhdlVisitor):
                  'signal_mode' : self.visit(ctx.signal_mode()),
                  'subtype_indication' : self.visit(ctx.subtype_indication()) }
 
+    def visitSignal_mode(self, ctx:vhdlParser.Signal_modeContext):
+        return ctx.getText()
+
+    def visitIdentifier_list(self, ctx:vhdlParser.Identifier_listContext):
+        return [self.visit(i) for i in ctx.identifier()]
+
     def visitIdentifier(self, ctx:vhdlParser.IdentifierContext):
+        return ctx.getText()
+
+    def visitName(self, ctx:vhdlParser.NameContext):
         return ctx.getText()
 
 def main(argv):
@@ -142,7 +194,7 @@ def main(argv):
     #printer = vhdlPrinter()
     #walker = antlr4.ParseTreeWalker()
     #walker.walk(printer, tree)
-    print(tree.toStringTree(None, parser))
+    #print(tree.toStringTree(None, parser))
                           
 if __name__ == '__main__':
     main(sys.argv)
